b60df324f0a8bcaddf7fee520b191b97a8c9fb61
Brittany Torres <btorres@aclanyc.com>
2018-01-30 16:32:09 +0000

    Done!

--- js/script.js
+++ js/script.js
@@ -5,41 +5,49 @@
 // CREATE THE FUNCTIONS BELOW
 
 
-
+/*global $*/
 // Document Ready Function. All of your jQuery should go in here. 
-$( document ).ready(function(){
-  
-  $('#try').click(function(){
-      var feed = $('input').val();
-      $('body').append(feed);
-  });
- 
-  function sentenceToWord(x){
-     var sent = x.split(" ");
-    return sent;
-  }
- console.log(sentenceToWord("Hello World "));
- 
- function wordsToPigLatin(words){
-    for(var i=0;i<words.length;i++){
-        words[i] = words[i]+ "ay";
-    }
- }
-});
+$(document).ready(function() {
+		
+	function setenceToPigLatin(){
+		// var vowels = ["a", "e", "i", "o", "u"]
+		var sentence = $("#userInput").val();
+		sentence = sentence.toString().toLowerCase();
+		var wordsArray = sentence.split(" ");
+		for(var i = 0; i < wordsArray.length; i++){
+			var char = wordsArray[i][0];
+			var secondChar = wordsArray[i][1];
+			var pigLatinized;
+			if(char==="a" || char === "e" || char === "i" || char === "o" || char === "u"){
+				pigLatinized = wordsArray[i] + "yay";
+			}
+			else if(char==="s" && secondChar==="h"){
+				var finalWord =wordsArray[i].substr(2);
+				pigLatinized = finalWord+char+secondChar+"ay";
+			}
+			else{
+				finalWord =wordsArray[i].substr(1);
+				pigLatinized = finalWord+char+"ay";
+			}
+			$("#result").append(pigLatinized + " ");
+		}
+	}
 
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
 
-
- function wordToPigLatin(x){
-      var pig = x + "ay";
-    return pig; 
-  }
-    $("try").click(function(){
-        wordToPigLatin();
-    }
-
+$("#userInput").keyup(function(event) {
+        if (event.keyCode === 13) {
+            $("#translate").click();
+        }
+    });
+	
+	$("#translate").click(function(){
+		$("#result").html("");
+		setenceToPigLatin();
+	});
+});
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
 	//Loops through all the words in the sentence and transforms each word
